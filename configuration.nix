f43e1f48bdadb738e74bcdb6a1d231a567f9739d
installer <installer@gmail.com>
2025-04-06 23:19:34 -0400

    Initial Commit

---
+++ configuration.nix
@@ -0,0 +1,359 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "nixos-bgrt";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        nixos-bgrt-plymouth
+      ];
+    };
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=2"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+
+    # Use XanMod kernel
+    kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
+
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader = {
+      systemd-boot.enable = true;
+      timeout = 0;
+    };
+    kernel.sysctl = {
+      "vm.swappiness" = 20;
+    };
+    initrd.systemd.enable = true;
+  };
+
+  networking.hostName = "nyx"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = false;
+  programs.xwayland.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.theme = "where_is_my_sddm_theme";
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dgnr = {
+    isNormalUser = true;
+    description = "Dreamgineer";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+    ];
+    packages = with pkgs; [
+      kdePackages.kate
+      kdePackages.kdeconnect-kde
+      gearlever
+      appimage-run
+      google-chrome
+      git
+      vesktop
+      fastfetch
+      nil
+      nixfmt-rfc-style
+      element-desktop
+      blackbox-terminal
+      micro
+      gh
+      spotube
+      inshellisense
+      #  thunderbird
+    ];
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "dgnr";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable auto-upgrades.
+  system.autoUpgrade = {
+    enable = true;
+    # Run daily
+    dates = "daily";
+    # Build the new config and make it the default, but don't switch yet.  This will be picked up on reboot.  This helps
+    # prevent issues with OpenSnitch configs not well matching the state of the system.
+    operation = "boot";
+  };
+
+  # Limit nix rebuilds priority.  When left on the default is uses all available reouses which can make the system unusable
+  nix = {
+    daemonCPUSchedPolicy = "idle";
+    daemonIOSchedClass = "idle";
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
+    vscode
+    plymouth
+    wineWowPackages.full
+    distrobox
+    bun
+    zulu23
+    ntfs3g
+    kdePackages.qtdeclarative
+    ktailctl
+    nodejs_20
+
+    where-is-my-sddm-theme
+    # Hyprland
+    kitty
+    wofi
+    hyprpaper
+    hyprpolkitagent
+    hyprshot
+    waybar
+    nerd-fonts.code-new-roman
+    gnome-keyring
+    hyprlock
+    hypridle
+    pavucontrol
+    brightnessctl
+  ];
+
+  environment.plasma6.excludePackages = with pkgs; [ kdePackages.krdp ];
+
+  services.flatpak.enable = true;
+
+  virtualisation.docker.enable = true;
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+  virtualisation.waydroid.enable = true;
+
+  # Networking
+  services.tailscale.enable = true;
+  # "100.100.100.100" "45.90.28.164" "45.90.30.164"
+  networking.nameservers = [
+    "8.8.8.8"
+    "1.1.1.1"
+  ];
+  networking.search = [
+    "tail71b97a.ts.net"
+    "dgnr.us"
+  ];
+  networking.firewall.enable = false;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+  # Nvidia Driver
+
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  zramSwap.enable = true;
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 8 * 1024;
+      priority = 1;
+    }
+  ];
+
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+  };
+
+  systemd.services."docker" = {
+    overrideStrategy = "asDropin";
+    unitConfig.After = lib.mkForce "docker.socket firewalld.service containerd.service time-set.target";
+  };
+  systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+
+  hardware.bluetooth.enable = true;
+
+  fileSystems = {
+    "/mnt/data" = {
+      device = "/dev/disk/by-uuid/D83224E63224CB78";
+      fsType = "ntfs3";
+      options = [ "nofail" ];
+    };
+  };
+
+  services.avahi = {
+    nssmdns4 = true;
+    enable = true;
+    ipv4 = true;
+    ipv6 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
+
+  # Development tools
+  services.lorri.enable = true;
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/dgnr/nix";
+  };
+
+  # Hyprland
+  programs.hyprland.enable = true;
+  services.fprintd = {
+    enable = true;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # nix-index stuff
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
+    wpilib.url = "github:frc4451/frc-nix";
+    thorium.url = "git+file:///home/dgnr/Scripts/nix-thorium";
+    nix-alien.url = "github:thiagokokada/nix-alien";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nix-index-database,
+      wpilib,
+      thorium,
+      ...
+    }:
+    {
+      # replace 'nyx' with your hostname here.
+      nixosConfigurations.nyx = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
+        specialArgs = { inherit self system; };
+        modules = [
+          ./configuration.nix
+
+          # comma & nix-index
+          nix-index-database.nixosModules.nix-index
+          { programs.nix-index-database.comma.enable = true; } # Add WPILib packages
+          (
+            {
+              pkgs,
+              ...
+            }:
+            {
+              environment.systemPackages = [
+                wpilib.packages.${system}.glass
+                wpilib.packages.${system}.advantagescope
+                wpilib.packages.${system}.vscode-wpilib
+                thorium.packages.${system}.default
+              ];
+            }
+          )
+
+          # nix-alien
+          (
+            { self, pkgs, ... }:
+            {
+              nixpkgs.overlays = [
+                self.inputs.nix-alien.overlays.default
+              ];
+              environment.systemPackages = with pkgs; [
+                nix-alien
+              ];
+              # Optional, needed for `nix-alien-ld`
+              programs.nix-ld.enable = true;
+            }
+          )
+        ];
+      };
+    };
+}
