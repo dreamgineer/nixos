74d6d5b31600d0a975f849373220b7ac8f0a23a9
Dreamgineer <me@dgnr.us>
2025-04-16 18:12:49 -0400

    Fix Waybar config

--- modules/home/hyprland/waybar.nix
+++ modules/home/hyprland/waybar.nix
@@ -1,112 +1,114 @@
 { config, ... }:
 {
   programs.waybar.enable = true;
-  programs.waybar.settings = {
-    layer = "top"; # Waybar at top layer
-    "position" = "bottom"; # Waybar at the bottom of your screen
-    "reload_style_on_change" = true;
-    height = 18; # Waybar height
-    # width = 1366; // Waybar width
-    # Choose the order of the modules
-    modules-left = [
-      "hyprland/workspaces"
-      "custom/spotify"
-    ];
-    modules-center = [ "hyprland/window" ];
-    modules-right = [
-      "pulseaudio"
-      "network"
-      "cpu"
-      "memory"
-      "battery"
-      "tray"
-      "clock"
-    ];
-    "hyprland/workspaces" = {
-      format = "{icon}";
-      format-icons = {
-        active = "";
-        default = "";
-        "empty" = "";
-      };
-      persistent-workspaces = {
-        "*" = [
-          1
-          2
-          3
-          4
-          5
-        ];
-      };
-    };
-    tray = {
-      # icon-size = 21;
-      "spacing" = 5;
-    };
-    clock = {
-      format-alt = "{:%Y-%m-%d}";
-    };
-    cpu = {
-      format = "{usage}% ";
-    };
-    memory = {
-      format = "{}% ";
-    };
-    battery = {
-      bat = "BAT0";
-      states = {
-        # good = 95;
-        warning = 30;
-        critical = 15;
-      };
-      format = "{capacity}% {icon}";
-      format-good = ""; # An empty format will hide the module
-      format-full = "";
-      format-icons = [
-        "󰁻"
-        "󰁼"
-        "󰁾"
-        "󰂀"
-        "󰂂"
-        "󰁹"
+  programs.waybar.settings = [
+    {
+      layer = "top"; # Waybar at top layer
+      "position" = "bottom"; # Waybar at the bottom of your screen
+      "reload_style_on_change" = true;
+      height = 18; # Waybar height
+      # width = 1366; // Waybar width
+      # Choose the order of the modules
+      modules-left = [
+        "hyprland/workspaces"
+        "custom/spotify"
       ];
-    };
-    "network" = {
-      format-wifi = "";
-      format-ethernet = "";
-      format-disconnected = "";
-      tooltip-format-disconnected = "Error";
-      tooltip-format-wifi = "{essid} ({signalStrength}%) ";
-      tooltip-format-ethernet = "{ifname} 🖧 ";
-      on-click = "kitty nmtui";
-    };
-    "pulseaudio" = {
-      #scroll-step = 1;
-      format = "{volume}% {icon}";
-      format-bluetooth = "{volume}% {icon}";
-      format-muted = "";
-      format-icons = {
-        headphones = "";
-        handsfree = "";
-        headset = "";
-        phone = "";
-        portable = "";
-        car = "";
-        default = [
-          ""
-          ""
+      modules-center = [ "hyprland/window" ];
+      modules-right = [
+        "pulseaudio"
+        "network"
+        "cpu"
+        "memory"
+        "battery"
+        "tray"
+        "clock"
+      ];
+      "hyprland/workspaces" = {
+        format = "{icon}";
+        format-icons = {
+          active = "";
+          default = "";
+          "empty" = "";
+        };
+        persistent-workspaces = {
+          "*" = [
+            1
+            2
+            3
+            4
+            5
+          ];
+        };
+      };
+      tray = {
+        # icon-size = 21;
+        "spacing" = 5;
+      };
+      clock = {
+        format-alt = "{:%Y-%m-%d}";
+      };
+      cpu = {
+        format = "{usage}% ";
+      };
+      memory = {
+        format = "{}% ";
+      };
+      battery = {
+        bat = "BAT0";
+        states = {
+          # good = 95;
+          warning = 30;
+          critical = 15;
+        };
+        format = "{capacity}% {icon}";
+        format-good = ""; # An empty format will hide the module
+        format-full = "";
+        format-icons = [
+          "󰁻"
+          "󰁼"
+          "󰁾"
+          "󰂀"
+          "󰂂"
+          "󰁹"
         ];
       };
-      on-click = "pavucontrol";
-    };
-    "custom/spotify" = {
-      format = " {}";
-      max-length = 40;
-      interval = 30; # Remove this if your script is endless and write in loop
-      exec = "$HOME/.config/waybar/mediaplayer.sh 2> /dev/null"; # Script in resources folder
-      exec-if = "pgrep spotify";
-    };
-  };
+      "network" = {
+        format-wifi = "";
+        format-ethernet = "";
+        format-disconnected = "";
+        tooltip-format-disconnected = "Error";
+        tooltip-format-wifi = "{essid} ({signalStrength}%) ";
+        tooltip-format-ethernet = "{ifname} 🖧 ";
+        on-click = "kitty nmtui";
+      };
+      "pulseaudio" = {
+        #scroll-step = 1;
+        format = "{volume}% {icon}";
+        format-bluetooth = "{volume}% {icon}";
+        format-muted = "";
+        format-icons = {
+          headphones = "";
+          handsfree = "";
+          headset = "";
+          phone = "";
+          portable = "";
+          car = "";
+          default = [
+            ""
+            ""
+          ];
+        };
+        on-click = "pavucontrol";
+      };
+      "custom/spotify" = {
+        format = " {}";
+        max-length = 40;
+        interval = 30; # Remove this if your script is endless and write in loop
+        exec = "$HOME/.config/waybar/mediaplayer.sh 2> /dev/null"; # Script in resources folder
+        exec-if = "pgrep spotify";
+      };
+    }
+  ];
   programs.waybar.style = ''
       * {
         border: none;
