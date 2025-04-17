fb3b7713581b93247ca6dc2741b244d8b6cd70c1
Dreamgineer <me@dgnr.us>
2025-04-17 03:01:31 -0400

    Swap insta & region screenshot

--- modules/home/hypr/default.nix
+++ modules/home/hypr/default.nix
@@ -165,8 +165,8 @@
       ", XF86AudioPlay, exec, playerctl play-pause"
       ", XF86AudioPrev, exec, playerctl previous"
       # Screenshot
-      ", print, exec, hyprshot -m output -m active --clipboard-only"
-      "SHIFT, print, exec, hyprshot -m region --clipboard-only"
+      ", print, exec, hyprshot -m region --clipboard-only"
+      "SHIFT, print, exec, hyprshot -m output -m active --clipboard-only"
     ];
     windowrulev2 = [
       # Ignore maximize requests from apps. You'll probably like this.
