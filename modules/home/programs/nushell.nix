84e1721bd2df65bfa3acb1de58aada402134a56c
Dreamgineer <me@dgnr.us>
2025-04-18 16:03:11 -0400

    Add inshellisense to nushell

--- modules/home/programs/nushell.nix
+++ modules/home/programs/nushell.nix
@@ -4,9 +4,17 @@
     enable = true;
     settings = {
       show_banner = false;
+      completions.algorithm = "fuzzy";
     };
     extraConfig = ''
       source ${builtins.toString ./assets/init.nu}
+      is
+      clear
+      if (($env.ISTERM? | default "0") != "1") { exit }
+    '';
+    extraEnv = ''
+      $env.PROMPT_COMMAND_RIGHT = ""
+      $env.PROMPT_INDICATOR = " "
     '';
     shellAliases = {
       a = "nix-alien";
